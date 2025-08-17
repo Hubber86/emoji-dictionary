2ddacca6dab0e86a2bbadc0810b57e5774476d54
Prajwal Bhimrao Kalashetty <110298413+Hubber86@users.noreply.github.com>
2025-08-17 22:30:59 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Allow frontend access
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
