d7194bcd3d8d60c9d712faf629edd317cf771e15
Prajwal Bhimrao Kalashetty <110298413+Hubber86@users.noreply.github.com>
2025-08-18 00:04:53 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -17,8 +17,7 @@ function App() {
     const fetchSuggestions = async () => {
       try {
         const res = await fetch(
-          // `https://emoji-dictionary.onrender.com/emoji?word=${searchWord}&category=${searchWord}`
-        `https://emoji-dictionary.onrender.com/emoji?word=${searchWord}`          
+          `https://emoji-dictionary.onrender.com/search?query=${word}`
         );
         const data = await res.json();
         setSuggestions(data.results || []);
